afceeac3a6585c5f8b75d0b1ec20f215fa88f1db
Shubham Gour <shubhamgour4616@gmail.com>
2024-02-10 17:30:57 +0530

    added code to clone git repos

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import simpleGit from "simple-git";
 
 const app = express();
 
@@ -7,14 +8,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 function generateSessionId() {
-  const timestamp = Date.now().toString(36); 
+  const timestamp = Date.now().toString(36);
   const randomPart = Math.random().toString(36).substr(2, 5);
-  const sessionId = timestamp + randomPart; 
+  const sessionId = timestamp + randomPart;
   return sessionId;
 }
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, Express4!");
+app.get("/", async (req: Request, res: Response) => {
+  const repoUrl = "https://github.com/ShubhamGour4616/deployer.git";
+  const id = generateSessionId();
+  const clonePath = `repos/${id}`;
+  try {
+    await simpleGit().clone(repoUrl, clonePath, { "--branch": "master" });
+    res.json({
+      id: id,
+      message: "Repository cloned successfully!",
+    });
+  } catch (error) {
+    res.status(500).send(`Failed to clone repository: ${error}`);
+  }
 });
 
 app.post("/", (req: Request, res: Response) => {
